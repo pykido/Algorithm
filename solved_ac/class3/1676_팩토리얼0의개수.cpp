4d53000c297ab91645450b37f1986131ffb97c00
pykido <77539625+pykido@users.noreply.github.com>
2022-07-02 11:41:52 +0900

    Create 1676_팩토리얼0의개수.cpp
     자리수를 결정하는 것은 10이 몇 번 곱해졌는지이다. 2의 배수보다는 5의 배수가 압도적으로 적기에 5의 배수의 수만큼이 답임을 알 수 있다.

---
+++ solved_ac/class3/1676_팩토리얼0의개수.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+
+using namespace std;
+
+int main(void) {
+    int N;
+    scanf("%d", &N);
+
+    int result = 0;
+    for (int i = 1; i <= N; i++) {
+        if (i%5 == 0) {
+            int k = i;
+            while (true) {
+                if (k % 5 != 0) {
+                    break;
+                } else {
+                    k /= 5;
+                    result += 1;
+                }
+            }
+        }
+    }
+    printf("%d", result);
+
+    return 0;
+}
